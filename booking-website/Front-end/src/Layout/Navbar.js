90c44e38e08ca97a52c66be68729c2c8c641bf56
tasnembaibars <tasnembaibars@gmail.com>
2022-07-07 16:14:04 +0300

    main

--- booking-website/Front-end/src/Layout/Navbar.js
+++ booking-website/Front-end/src/Layout/Navbar.js
@@ -53,7 +53,7 @@ function Navbar() {
             <nav className="navbar navbar-expand-lg">
               <div className="container">
                 <a className="navbar-brand" href="/"
-                  ><img src="img/logo.png" alt="logo" /></a
+                  ><img src="img/logo.png" alt="logo" height={"90px"} width={"150px"} /></a
                 ><button
                   className="navbar-toggler navbar-toggler-right collapsed"
                   type="button"
@@ -231,7 +231,7 @@ function Navbar() {
                       <a className="nav-link" href="/contact">Contact</a>
                     </li>
                   </ul>
-                  <a className="nav-link" href="/profile" style={{color:"white"}}><svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                  <a className="nav-link" href="/profile" style={{color:"white"}}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
   <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
   <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
 </svg></a>
