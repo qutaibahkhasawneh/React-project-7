4ab8fa25f626ce45aca04462ebc59d2b725a4f8d
tasnembaibars <tasnembaibars@gmail.com>
2022-07-08 22:30:43 +0300

    main

--- booking-website/Front-end/src/components/profile/Profile.js
+++ booking-website/Front-end/src/components/profile/Profile.js
@@ -29,6 +29,9 @@ axios.put(`http://127.0.0.1:8000/api/users/1`, users)
 .then((res)=>setUsers(res.data))
 .then(()=>setUpdate(!update))
 console.log(users)
+if(!update){
+window.alert('Your profile has been uploaded successfuly')
+}
 
 }
 
@@ -163,18 +166,17 @@ console.log(users)
 
                     </div>
                   </div>
-                  <div className="col-md-6">
-                    </div>
+                  
                   <div className="col-md-6">
                     <div className="form-group">
-                      <label>Reset Password</label>
+                      <label>Password</label>
                       <input type="password" className="form-control" name='password'
                        value={users.password} 
                       onChange={(e)=>setUsers({...users,password:e.target.value})} />
                      
                 </div>
                   </div> 
-                  <div className="col-md-6">
+                  {/* <div className="col-md-6">
                     <div className="form-group">
                       <label>Confirm Password</label>
                       <input type="password" className="form-control" name='password'
@@ -182,7 +184,7 @@ console.log(users)
                       onChange={(e)=>setUsers({...users,password:e.target.value})} />
                      
                 </div>
-                  </div> 
+                  </div>  */}
                
                
                        
